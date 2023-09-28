c73c6fb3cf1e6633b2150751a14272b053cb22c1
Kobakk <103675092+Kobakk@users.noreply.github.com>
2023-09-28 17:17:22 +0200

    Add files via upload

--- StartPage/main.js
+++ StartPage/main.js
@@ -1,17 +1,15 @@
 function currentTime() {
-    console.log("Script cargado")
     let date = new Date();
     let hh = date.getHours();
     let mm = date.getMinutes();
 
-    let time = hh + ":" + mm ;
+    let time = hh + ":" + mm;
 
     document.getElementById("reloj").innerText = time;
-    
     let t = setTimeout(function () { currentTime() }, 1000);
 }
 
+
 document.addEventListener('DOMContentLoaded', function () {
-    // Llama a la función correcta
     currentTime();
 });
