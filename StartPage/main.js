a58c204e44ad10111096a16f1e828c6a5bbbed42
Sev <103675092+Kobakk@users.noreply.github.com>
2024-01-24 13:08:07 +0100

    Reloj marcand la hora cada segundo
    La anterior manera era menos practica  y mostraba de manera rara las horas cuando eran menos de 10

--- StartPage/main.js
+++ StartPage/main.js
@@ -1,15 +1,31 @@
-function currentTime() {
-    let date = new Date();
-    let hh = date.getHours();
-    let mm = date.getMinutes();
+// function currentTime() {
+//     let date = new Date();
+//     let hh = date.getHours();
+//     let mm = date.getMinutes();
 
-    let time = hh + ":" + mm;
+//     let time = hh + ":" + mm;
 
-    document.getElementById("reloj").innerText = time;
-    let t = setTimeout(function () { currentTime() }, 1000);
-}
+//     document.getElementById("reloj").innerText = time;
+//     let t = setTimeout(function () { currentTime() }, 1000);
+// }
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    currentTime();
+// document.addEventListener('DOMContentLoaded', function () {
+//     currentTime();
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+    let elemento = document.getElementById("hora");
+
+    let time = setInterval(() => {
+        var date = new Date();
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var seconds = date.getSeconds();
+        if (minutes < 10) minutes = "0" + minutes;
+        if (seconds <10) seconds = "0" + seconds;
+        if (hours < 10 ) hours = "0" + hours;
+        elemento.textContent = `${hours} : ${minutes} : ${seconds} `;
+    }, 1000);
 });
+
